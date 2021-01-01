06466fe548f74e2f998cab8f335fc1070cdc725a
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-01-01 11:53:13 -0300

    feat(rocket-league): add launch and close handlers

--- js/rocket-league.js
+++ js/rocket-league.js
@@ -7,4 +7,26 @@ class RocketLeague {
     this.logger.info('Extension Started', '')
     this.logger.info('WLED', `Assigned IP ${this.wled.ip}`)
   }
+
+  launched() {
+    this.logger.info('Rocket League', 'Launched')
+    this.lobby()
+  }
+
+  running() {
+    this.logger.info('Rocket League', 'Running')
+    this.lobby()
+  }
+
+  closed() {
+    this.logger.info('Rocket League', 'Closed')
+    this.wled.request({ PL: 1 })
+
+    setTimeout(() => { overwolf.windows.getMainWindow().close() }, 5000)
+  }
+
+  lobby() {
+    this.logger.info('Info', 'Waiting for game')
+    this.wled.request({ CL: 'he04a00', C2: 'h1400d6', C3: 'h000000', FX: 37, SX: 20, IX: 190 })
+  }
 }
