4a81c1f970d072053f82070c2ca7c086a27872ef
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-01-01 11:54:32 -0300

    feat(rocket-league): add set blue and orange team

--- js/rocket-league.js
+++ js/rocket-league.js
@@ -29,4 +29,24 @@ class RocketLeague {
     this.logger.info('Info', 'Waiting for game')
     this.wled.request({ CL: 'he04a00', C2: 'h1400d6', C3: 'h000000', FX: 37, SX: 20, IX: 190 })
   }
+
+  setBlueTeam() {
+    this.info.team = 'blue'
+    this.inGame()
+  }
+
+  setOrangeTeam() {
+    this.info.team = 'orange'
+    this.inGame()
+  }
+
+  inGame() {
+    this.logger.info('Game', `Playing on ${this.info.team || '<unknown>'} team`)
+
+    if (this.info.team === 'blue') {
+      this.wled.request({ CL: 'h1400d6', C2: 'h002b6e', C3: 'h000000', FX: 106, SX: 210, IX: 170 })
+    } else if (this.info.team === 'orange') {
+      this.wled.request({ CL: 'he04a00', C2: 'h803302', C3: 'h000000', FX: 106, SX: 210, IX: 170 })
+    }
+  }
 }
