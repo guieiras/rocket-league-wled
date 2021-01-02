2c22bb2b02acc2a9f356f28566b8e28f11d299a1
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-01-01 23:12:22 -0300

    lint: fix deprecation warning and errors

--- odk-ts/ow-window.ts
+++ odk-ts/ow-window.ts
@@ -10,94 +10,96 @@ export class OWWindow {
   }
 
   public async restore(): Promise<void> {
-    let that = this;
+    let instance = this;
 
     return new Promise<void>(async (resolve) => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
+      let id: string = <string>instance._id;
       overwolf.windows.restore(id, result => {
-        if (!result.success)
+        if (!result.success) {
           console.error(`[restore] - an error occurred, windowId=${id}, reason=${result.error}`);
+        }
+
         resolve();
       });
-    })
+    });
   }
 
   public async minimize(): Promise<void> {
-    let that = this;
+    let instance = this;
 
     return new Promise<void>(async resolve => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
+      let id: string = <string>instance._id;
       overwolf.windows.minimize(id, () => { });
       return resolve();
-    })
+    });
   }
 
   public async maximize(): Promise<void> {
-    let that = this;
+    let instance = this;
 
     return new Promise<void>(async resolve => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
+      let id: string = <string>instance._id;
       overwolf.windows.maximize(id, () => { });
       return resolve();
-    })
+    });
   }
 
   public async hide(): Promise<void> {
-    let that = this;
+    let instance = this;
 
     return new Promise<void>(async resolve => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
+      let id: string = <string>instance._id;
       overwolf.windows.hide(id, () => { });
       return resolve();
-    })
+    });
   }
 
-  public async close() {
-    let that = this;
+  public async close(): Promise<void> {
+    let instance = this;
 
     return new Promise(async resolve => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
 
       const result = await this.getWindowState();
 
-      if (result.success &&
-        (result.window_state !== 'closed')) {
+      if (result.success && (result.window_state !== 'closed')) {
         await this.internalClose();
       }
 
-      return resolve();
-    })
+      resolve();
+    });
   }
 
   public dragMove(elem: HTMLElement) {
     elem.className = elem.className + ' draggable';
-    elem.onmousedown = e => {
+    elem.onmousedown = (e) => {
       e.preventDefault();
       overwolf.windows.dragMove(this._name);
     };
   }
 
   public async getWindowState(): Promise<GetWindowStateResult> {
-    let that = this;
+    let instance = this;
 
     return new Promise<GetWindowStateResult>(async resolve => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
+
+      let id: string = <string>instance._id;
+
       overwolf.windows.getWindowState(id, resolve);
-    })
+    });
   }
 
   public static async getCurrentInfo(): Promise<OwWindowInfo> {
     return new Promise<OwWindowInfo>(async resolve => {
       overwolf.windows.getCurrentWindow(result => {
         resolve(result.window);
-      })
-    })
+      });
+    });
   }
 
   private obtain(): Promise<OwWindowInfo | null> {
@@ -106,13 +108,12 @@ export class OWWindow {
         if (res && res.status === "success" && res.window && res.window.id) {
           this._id = res.window.id;
 
-          if (!this._name) {
-            this._name = res.window.name;
-          }
+          if (!this._name) { this._name = res.window.name; }
 
           resolve(res.window);
         } else {
           this._id = null;
+
           reject();
         }
       };
@@ -126,26 +127,25 @@ export class OWWindow {
   }
 
   private async assureObtained(): Promise<void> {
-    let that = this;
+    let instance = this;
+
     return new Promise<void>(async resolve => {
-      await that.obtain();
+      await instance.obtain();
+
       return resolve();
     });
   }
 
   private async internalClose(): Promise<void> {
-    let that = this;
+    let instance = this;
 
     return new Promise<void>(async (resolve, reject) => {
-      await that.assureObtained();
-      let id: string = <string>that._id;
+      await instance.assureObtained();
+      let id: string = <string>instance._id;
 
       overwolf.windows.close(id, res => {
-
-        if (res && res.success)
-          resolve();
-        else
-          reject(res);
+        if (res && res.success) { resolve(); }
+        else { reject(res); }
       });
     })
   }
